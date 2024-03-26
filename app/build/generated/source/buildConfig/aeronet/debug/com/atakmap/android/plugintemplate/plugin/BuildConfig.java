94c6c9b08bb3af40ecabf039c24716bcc2c2aa51
Your Name <you@example.com>
2024-03-26 12:17:19 -0700

    cool it renders

---
+++ app/build/generated/source/buildConfig/aeronet/debug/com/atakmap/android/plugintemplate/plugin/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.atakmap.android.plugintemplate.plugin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.atakmap.android.plugintemplate.plugin.aeronet";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "aeronet";
+  public static final int VERSION_CODE = 1711472427;
+  public static final String VERSION_NAME = "1.0 (2d7dc090) - [5.1.0]";
+  // Field from the variant API
+  public static final String ATAK_PACKAGE_NAME = "com.atakmap.app.civ";
+}
